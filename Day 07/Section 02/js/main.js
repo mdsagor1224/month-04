f971b0ff0dd71c7e3b6af7b20a935b6bb1f90195
mdsagor <mdsagorahmed1224@gmail.com>
2024-01-03 21:51:46 +0600

    section 02

---
+++ Day 07/Section 02/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        items:1,
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        nav:true,
+        navText:["<i class='fa-solid fa-angle-left'></i>", "<i class='fa-solid fa-angle-right'></i>"]
+    })
+  });
